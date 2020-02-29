c946552c670f0cb35f15214dd709cffdee6ab9fd
bushanka <61285033+bushanka@users.noreply.github.com>
2020-03-01 00:33:15 +0300

    Create dir with ice-version 2

---
+++ Cell_version_2/Cage.py
@@ -0,0 +1,44 @@
+class Cage:
+    """This class describe one cell"""
+    temperature = None
+    material = None
+    mass = None
+    upd_q = None  # q that cell get from other cells
+    saved_q = None  # q that goes to change material
+    fraction = 0  # parameter that is used for change effective_mass
+    checker_transformation = True  # exit criteria
+    # const
+    dx = 10 ** (-3)
+    S = 10 ** (-6)
+    t_0 = 10 ** (-3)
+    V = S * dx
+
+    def interaction(self, *cell_list):
+        for i in range(len(cell_list)):
+
+            self.upd_q += 0.5 * (self.material.conductivity + cell_list[i].material.conductivity) * (
+                    self.temperature - cell_list[i].temperature) / Cage.dx * Cage.t_0 * Cage.S  # q that taken from cell
+
+            if self.saved_q >= self.material.fusion * self.mass * (
+                    1 - self.fraction):  # check if saved_q >= lambda * effective_mass
+                self.checker_transformation = False  # exit if False
+                self.fraction = 0  # change to initial
+                self.material = cell_list[i].material  # change material
+                self.saved_q = 0  # change to initial
+                self.V = self.mass / self.material.density  # Mass conservation law
+            if round(self.temperature,
+                     1) == self.material.trans_temperature and self.checker_transformation:  # check criterion and temp to enter
+                self.saved_q += abs(self.upd_q) * 10
+                self.fraction = self.saved_q / (self.material.fusion * self.mass * (1 - self.fraction))
+                self.upd_q = 0
+
+    def change_temp(self):
+        self.temperature -= self.upd_q / (self.material.capacity * self.mass)
+        self.upd_q = 0
+
+    def __init__(self, temperature=None, material=None):
+        self.temperature = temperature
+        self.material = material
+        self.mass = material.density * Cage.S * Cage.dx
+        self.upd_q = 0
+        self.saved_q = 0
---
+++ Cell_version_2/main(bad).py
@@ -0,0 +1,36 @@
+import Cage as Cg
+import Materials
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+# plot and show graphs
+def plotter(number, result):
+    for b in range(number):
+        plt.plot(result[:, b])
+    plt.show()
+
+
+n = 5500
+first = Cg.Cage(5, Materials.water)
+second = Cg.Cage(-30, Materials.ice)
+third = Cg.Cage(5, Materials.water)
+# first = Cg.Cage(3, Materials.water)
+# second = Cg.Cage(-50, Materials.ice)
+# third = Cg.Cage(2, Materials.water)
+cells_l = [first, second, third]
+number_of_cells = np.zeros((n, len(cells_l)))
+for i in range(n):
+    number_of_cells[i][0] = first.temperature
+    number_of_cells[i][1] = second.temperature
+    number_of_cells[i][2] = third.temperature
+    first.interaction(second)
+    second.interaction(first)
+    third.interaction(second, first)
+    third.change_temp()
+    first.change_temp()
+    second.change_temp()
+
+plotter(len(cells_l), number_of_cells)
+print(first.material.name, second.material.name, third.material.name)
+print(number_of_cells[-1])
